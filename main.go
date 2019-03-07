af753db8fee330e66c0e91e09f3ca03040b50a1e
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2019-03-06 23:29:35 -0800

    Building Slackbot real-time MSGing detection

--- main.go
+++ main.go
@@ -13,11 +13,27 @@ import (
 	"github.com/nlopes/slack"
 )
 
+// Initializing the Slack Client API
 var (
 	slackClient *slack.Client
 )
 
 // Main run function
 func main() {
+	// Instantiating our Slack client to communicate with Make School's Slack
 	slackClient = slack.New(os.Getenv("SLACK_ACCESS_TOKEN"))
+
+	// Instantiating real-time messaging with our Slackbot
+	realTimeMSG := slackClient.NewRTM()
+
+	// Wrapping our RTM connection in a concurrent Go Routine
+	go realTimeMSG.ManageConnection()
+
+	// Check for real-time messages hitting the Slackbot
+	for msg := range realTimeMSG.IncomingEvents() {
+		switch event := msg.Data.(type) {
+		case *slack.MessageEvent:
+			// TODO: Create separate Go Routine to handle Slack messages
+		}
+	}
 }
